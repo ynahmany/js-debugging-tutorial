8feab010c878e0e9d72721d200546d88b3e5a25f
ynahmany <yohainjug@gmail.com>
2020-03-04 08:51:38 +0200

    adding pic for debugger

--- console/index.js
+++ console/index.js
@@ -4,6 +4,7 @@ const DEBUG_STOP_POINT = 1337;
 function debuggerLoop() {
     for(let i=0; i < LOOP_ITERATION_NUMBER; i++) {
         if (i === DEBUG_STOP_POINT) {
+            console.log('Check out the image!')
             debugger;
         }
     }
